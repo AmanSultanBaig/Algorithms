4da2706b53410c4e5a91320ceb301efa1aa83374
Aman Sultan Baig <amansultanbaig@gmail.com>
2022-01-03 20:06:10 +0500

    Update ATM-Algorithms.js

--- ATM-Algorithms.js
+++ ATM-Algorithms.js
@@ -1,18 +1,17 @@
-
-	function Withdrawal(amount) {
+function Withdrawal(amount) {
     	let transactionCharges = 0.05;
         let userBalance = 5000;
 
         if(amount % 5 === 0){
         	if(amount + transactionCharges <= userBalance){
         		userBalance = amount - transactionCharges;
-            	console.log(`Your Current Balance ${userBalance}`)
+            		console.log(`Your Current Balance ${userBalance}`)
         	} else {
-            	console.log(`You have insufficient balance to perform transaction.`)
+            		console.log(`You have insufficient balance to perform transaction.`)
         	}
        	} else {
             console.log(`Error Invalid input for transaction.`)
         }
     }
     
-    Withdrawal(1000)
+Withdrawal(1000)
